52c957063025c6ca797fbace7d0d1e022ca26058
Student <student@lan.itstep.kiev.ua>
2021-02-01 21:42:03 +0200

    lesson_pr1

---
+++ rainbow.py
@@ -0,0 +1,21 @@
+color = int(input('input color 1 - 6 '))
+col = {
+    1: 'white',
+    2: 'blue',
+    3: 'green',
+    4: 'red',
+    5: 'yellow',
+    6: 'orange'
+}
+
+if color >= 1 or color <=6:
+    print(col[color], col[color - 1], col[color + 1])
+else:
+    print("input color 1 to 6!!")
+
+
+
+
+
+
+
